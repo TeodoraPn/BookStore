17ce90db9fb288e647bea6d2fbdd86ad78babb00
Teodora Pinzariu <teodorapinzariu@teodoras-mbp.home>
2022-11-16 22:01:35 +0200

    Added new pages

--- UI/src/Screens/SignInScreen/SignInScreen.js
+++ UI/src/Screens/SignInScreen/SignInScreen.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import { View, Text, Image, ImageBackground } from 'react-native'
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput, Image, ImageBackground } from 'react-native';
+import CustomInput from '../../components/CustomInput/CustomInput.js';
+import CustomButton from '../../components/CustomButton/CustomButton.js';
+import axios from 'axios';
+import {useNavigation} from '@react-navigation/native';
+
+const url = 'http://localhost:8080';
 
 const SignInScreen = () => {
 
-    return(
-        <View>
-            
-        </View>
-    )
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigation = useNavigation();
+
+  const onSignInPressed = () => {
+    console.warn('Sign in');
+    navigation.navigate('CustomerHomeScreen');
+    axios
+      .post(`http://10.0.2.2:8080/signin`, {email, password})
+      .then(res => console.log(JSON.stringify(res.data)))
+      .catch(err => console.log('err: ' + err));
+  };
+
+  const onCreateAccount = () => {
+    console.warn('Create account');
+    navigation.navigate('SignUp');
+  };
+
+  return (
+    <View style={styles.root}>
+      <CustomInput placeholder="Gmail or Username" value={email} setValue={setEmail} />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
+      <CustomButton text="Sign In" onPress={onSignInPressed} />
+      <CustomButton text="Don’t have an account? Sign up now" onPress={onCreateAccount} />
+    </View>
+  );
+
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center', 
+    padding: 50, 
+    backgroundColor: '#222D31',
+  },
+});
 
 export default SignInScreen
---
+++ UI/src/navigation/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import IntroScreen from '/Users/teodorapinzariu/BookStore/Bookstore/UI/src/Screens/IntroScreen/IntroScreen.js';
+import SignInScreen from '/Users/teodorapinzariu/BookStore/Bookstore/UI/src/Screens/SignInScreen/SignInScreen.js';
+import SignUpScreen from '/Users/teodorapinzariu/BookStore/Bookstore/UI/src/Screens/SignUpScreen/SignUpScreen.js';
+import CustomerHomeScreen from '/Users/teodorapinzariu/BookStore/Bookstore/UI/src/Screens/CustomerHomeScreen/CustomerHomeScreen.js';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Intro" component={IntroScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="CustomerHomeScreen" component={CustomerHomeScreen} />
+     </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
