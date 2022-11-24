8c4b805d00e39f787dd4517e94275a44ea601ca0
ioanacapra11 <102293162+ioanacapra11@users.noreply.github.com>
2022-11-24 21:37:09 +0200

    fixed register

--- BE/controllers/auth.js
+++ BE/controllers/auth.js
@@ -1,54 +1,57 @@
 const {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} = require("firebase/auth");
-
-const { firebaseApp } = require("../database.js");
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where,
+} = require("firebase/firestore");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { db, firebaseApp } = require("../database.js");
 
 exports.signup = async (req, res) => {
-  const auth = getAuth();
-  await createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      return res.status(200).json(user);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      if (errorCode == "auth/email-already-in-use") {
-        return res.status(409).send("user with email already exists");
-      }
-      if (errorCode == "auth/weak-password") {
-        return res.status(400).send("the password is too weak");
-      }
-      return res.status(401).json({
-        errorCode,
-        errorMessage,
-      });
+  try {
+    const body = req.body;
+    // get all users from db
+    const usersCollection = collection(db, "users");
+    const queryUsers = query(usersCollection);
+    const usersDocs = await getDocs(queryUsers);
+    const allUsers = [];
+    usersDocs.forEach((user) => {
+      allUsers.push(user.data());
     });
-};
+    // TODO validate if the username already exists
+    const usernameAlreadyExists = allUsers.filter((user) => {
+      return user.username === body.username;
+    });
+    if (usernameAlreadyExists.length !== 0) {
+      return res.status(400).send("username already exists");
+    }
+    // TODO validate if the email already exists
+    const emailAlreadyExists = allUsers.filter((user) => {
+      return user.gmail === body.gmail;
+    });
+    if (emailAlreadyExists.length !== 0) {
+      return res.status(400).send("email already exists");
+    }
 
-exports.signin = async (req, res) => {
-  const auth = getAuth(firebaseApp);
-  await signInWithEmailAndPassword(auth, req.body.email, req.body.password)
-    .then(() => {
-      return res.status(200).send("user signed in successfully");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      if (errorCode == "auth/wrong-password") {
-        return res.status(403).send("wrong password");
-      }
-      if (errorCode == "auth/user-not-found") {
-        return res
-          .status(403)
-          .send(`user with email ${req.body.email} does not exist`);
-      }
-      return res.status(401).json({
-        errorCode,
-        errorMessage,
-      });
+    if (body.password !== body.confirmPassword) {
+      return res.status(400).send("passwords don't match");
+    }
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    const users = collection(db, "users");
+    await addDoc(users, {
+      role: body.role,
+      username: body.username,
+      gmail: body.gmail,
+      password: hashedPassword,
     });
+    console.log("account created");
+    res.status(201).send(`account created`);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("can't create account");
+  }
 };
