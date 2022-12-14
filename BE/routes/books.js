1470f52384659be660bf554e11bf75770a4c1718
ioanacapra11 <102293162+ioanacapra11@users.noreply.github.com>
2022-12-14 23:10:46 +0200

    implemented delete books

--- BE/routes/books.js
+++ BE/routes/books.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { addBook, getBooks } = require("../controllers/books.js");
+const {
+  addBook,
+  getBooks,
+  deleteBook,
+  //   updateBook,
+} = require("../controllers/books.js");
 
 router.post("/books", addBook);
 router.get("/books", getBooks);
-// router.delete("/books/:title", deleteBook);
+router.delete("/books/:title", deleteBook);
+// router.patch("/books/:title", updateBook);
 
 module.exports = router;
